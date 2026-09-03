9f1342b9ad814863291716cd5c291786321664a1
Felipe Rhydan Bezerra de Albuquerque <feliperhydan@academicos.com>
2026-09-03 14:37:17 -0300

    first commit

---
+++ src/test/java/felipe/rhydan/sistema_chamados/SistemaChamadosApplicationTests.java
@@ -0,0 +1,13 @@
+package felipe.rhydan.sistema_chamados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaChamadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
